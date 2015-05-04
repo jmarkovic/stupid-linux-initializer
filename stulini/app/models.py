ac50660e6b564e7ae211feebcafae2d6ca84d013
Josip Marković <josip@simplesource.hr>
2015-05-04 17:33:48 +0200

    initial project with Django 1.8

---
+++ stulini/app/models.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Base(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    icon = models.CharField(null=True, blank=True)
+    keyword = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+def ppa_link_validator(value):
+    if value[:4] is not "ppa:":
+        return ValidationError("PPA link has to start with 'ppa:'")
+
+
+class PPA(models.Model):
+    ppa_link = models.CharField(validators=[ppa_link_validator])
+    latest_version = models.CharField()
+
+    class Meta:
+        verbose_name = _("ppa")
+
+
+class Custom(models.Model):
+    setup_script_url = models.CharField()
+
+    class Meta:
+        verbose_name = _("custom")
